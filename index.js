c6d024c460aed94d4a959d39c4abc7de4ea01f0a
Bobby Hinton <bob.g.hint@gmail.com>
2022-05-26 14:19:41 -0500

    commit tree f1faa6d3ffbbb1827f8d278c7ef37c6fd8387807

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    console.log(string.toLowerCase())
+    return array.filter(driver => driver.substring(0,string.length).toLowerCase() === string.toLowerCase())
+  }
+
+  //string.length allows the function to look at the le
+
+function matchName(array,string) {
+    return array.filter(drivers => drivers.name === string)
+}
+
+
